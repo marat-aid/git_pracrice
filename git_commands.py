96120ae90247693eefa1003f4716443d61d7387b
Marat Aidaana <marat_ai@auca.kg>
2021-05-10 17:25:43 +0600

    initial commit

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+git init - .git 
+git remote add name  url
+git pull origin master
+git status
+git add
+git add .
+git commit -m "comments"
+git branch
+git branch name branch
+git checkout name branch
+git push name branch
+git push origin master
+git reset filename 
+
